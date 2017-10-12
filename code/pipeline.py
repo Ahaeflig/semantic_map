5abf732a9aa32a009bfe5cc19ce80e0beb29c7ee
ahaeflig <adan.haefliger@gmail.com>
2017-10-12 06:55:42 +0200

    ResNet test and vgg fixes p.1

--- code/pipeline.py
+++ code/pipeline.py
@@ -20,7 +20,7 @@ def get_K_repr(K, layer):
 
     content_maps = layer[0]
     reference_maps = layer[1] 
-    
+
     content_maps = content_maps.reshape(content_maps.shape[0]*content_maps.shape[1], content_maps.shape[2])
     reference_maps = reference_maps.reshape(reference_maps.shape[0]*reference_maps.shape[1], reference_maps.shape[2]).transpose()
     
@@ -29,8 +29,6 @@ def get_K_repr(K, layer):
     estimator = decomposition.NMF(n_components = K, init = 'random', tol=5e-3, random_state=0)    
     W = estimator.fit_transform(res)
     H = estimator.components_
-    
-    a = layer[0].shape[0]
     
     W = W.reshape(layer[0].shape[0], layer[0].shape[1], K)
     H = H.reshape(K, layer[1].shape[0], layer[1].shape[1])
@@ -69,7 +67,7 @@ def get_maps(img_content, img_context, K):
             conv4_1 = sess.run(vgg.conv4_1, feed_dict=feed_dict)
             conv4_2 = sess.run(vgg.conv4_2, feed_dict=feed_dict)
             conv4_3 = sess.run(vgg.conv4_3, feed_dict=feed_dict)
-            conv4_4 = sess.run(vgg.conv4_4, feed_dict=feed_dict)
+            
 
             conv5_1 = sess.run(vgg.conv5_1, feed_dict=feed_dict)
             conv5_2 = sess.run(vgg.conv5_2, feed_dict=feed_dict)
@@ -78,9 +76,11 @@ def get_maps(img_content, img_context, K):
             
             
             conv3_3 = sess.run(vgg.conv3_3, feed_dict=feed_dict)
+           
+            conv3_4 = sess.run(vgg.conv3_4, feed_dict=feed_dict)  
             '''
-            conv3_4 = sess.run(vgg.conv3_4, feed_dict=feed_dict)            
             
+            conv4_4 = sess.run(vgg.conv4_4, feed_dict=feed_dict)
             
             sess.close();
         
@@ -104,11 +104,12 @@ def get_maps(img_content, img_context, K):
     
     
     Ws_43, Hs_43 = get_K_repr(K, conv4_3)
-    Ws_44, Hs_44 = get_K_repr(K, conv4_4)
+   
     Ws_33, Hs_33 = get_K_repr(K, conv3_3)
-    '''
-    Ws_34, Hs_34 = get_K_repr(K, conv3_4)
     
+    Ws_34, Hs_34 = get_K_repr(K, conv3_4)
+    '''
+    Ws_44, Hs_44 = get_K_repr(K, conv4_4)
     
     '''
         'conv2_1' : [Ws_21, Hs_21],
@@ -117,11 +118,12 @@ def get_maps(img_content, img_context, K):
         'conv3_1' : [Ws_31, Hs_31],
         'conv3_2' : [Ws_32, Hs_32],   
         'conv3_3' : [Ws_33, Hs_33],
+        'conv3_4' : [Ws_34, Hs_34],  
     
         'conv4_1' : [Ws_41, Hs_41],
         'conv4_2' : [Ws_42, Hs_42],
         'conv4_3' : [Ws_43, Hs_43],
-        'conv4_4' : [Ws_44, Hs_44], 
+        
          
         'conv5_1' : [Ws_51, Hs_51],
         'conv5_2' : [Ws_52, Hs_52],
@@ -131,7 +133,8 @@ def get_maps(img_content, img_context, K):
     '''
        
     repr_dict = {
-        'conv3_4' : [Ws_34, Hs_34],   
+ 
+        'conv4_4' : [Ws_44, Hs_44], 
     }
 
     return OrderedDict(sorted(repr_dict.items()))
@@ -140,3 +143,44 @@ def get_maps(img_content, img_context, K):
     #prob = sess.run(vgg.prob, feed_dict=feed_dict)
     #print_prob(prob[0], VGG_PATH + 'vgg_classes.txt')
     #print_prob(prob[1], VGG_PATH + 'vgg_classes.txt')
+    
+    
+#courtesy of https://stackoverflow.com/questions/21030391/how-to-normalize-array-numpy
+def normalized(a, axis=-1, order=2):
+    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
+    l2[l2==0] = 1
+    return a / np.expand_dims(l2, axis)
+    
+def get_maps2(img_content, img_context, K):
+    
+    content_shape = img_content.shape
+    context_shape = img_context.shape
+    
+    batch1 = img_content.reshape(1, content_shape[0], content_shape[1], 3)
+    batch2 = img_context.reshape(1, context_shape[0], context_shape[1], 3)
+
+    tf.reset_default_graph()
+    with tf.device('/gpu:0'):
+        with tf.Session() as sess:
+
+            vgg = vgg19.Vgg19(VGG_PATH + 'vgg19.npy')
+            images = tf.placeholder("float", [1, None, None, 3])
+            vgg.build(images)
+            
+            feed_dict = {images: batch1}
+            feed_dict2 = {images: batch2}
+            conv4_4 = [sess.run(vgg.conv4_4, feed_dict=feed_dict)[0], sess.run(vgg.conv4_4, feed_dict=feed_dict2)[0]]     
+
+            #conv3_4 = sess.run(vgg.conv3_4, feed_dict=feed_dict)
+            sess.close();
+        
+  
+    Ws, Hs = get_K_repr(K, conv4_4)
+    #[array_s3[0], array_s3_2[0]]
+       
+    repr_dict = {
+        'conv4_4' : [normalized(Ws), normalized(Hs)],   
+    }
+
+    return OrderedDict(sorted(repr_dict.items()))
+    
